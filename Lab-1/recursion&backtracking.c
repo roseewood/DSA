dfcab6bf3728915f1d268f1ac0db9c01d99075af
roseewood <rw031106@gmail.com>
2025-09-27 10:27:43 +0530

    permutations

---
+++ Lab-1/recursion&backtracking.c
@@ -0,0 +1,6 @@
+// 3) Print all permutations of a string using recursion (e.g., "abc" → "abc", "acb", etc.).
+
+/* 
+Permutations: A permutation is a different arrangement of the characters in a string. 
+ e.g The permutations of "ABC" are: ABC, ACB, BAC, BCA, CAB, CBA.
+*/
