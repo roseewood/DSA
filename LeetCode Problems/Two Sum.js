4644b008a595bb69a768f244386c1ae2912560f1
roseewood <193557890+roseewood@users.noreply.github.com>
2026-05-26 21:11:36 +0530

    added solution

--- LeetCode Problems/Two Sum.js
+++ LeetCode Problems/Two Sum.js
@@ -32,3 +32,20 @@ Only one valid answer exists.
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  */
+
+var twoSum = function(nums, target) {
+
+    const map = new Map();
+
+    for(let i = 0; i < nums.length; i++) {
+
+        const complement = target - nums[i];
+
+        if(map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+
+};
